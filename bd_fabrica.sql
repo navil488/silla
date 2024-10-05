548cc1eebe420cda01b51e6fab9334cabe3ffa84
navil488 <109832610+navil488@users.noreply.github.com>
2024-10-05 05:08:55 -0600

    Update bd_fabrica.sql

--- bd_fabrica.sql
+++ bd_fabrica.sql
@@ -1,8 +1,44 @@
 CREATE TABLE DetallePedido (
     ID_Detalle INT AUTO_INCREMENT PRIMARY KEY,
     ID_Pedido INT,
-    ID_Silla INT,
+    ID_globos INT,
     Cantidad INT,
     PRECIO_TOTAL DECIMAl (10, 2)
+    ID_Sombrilla INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_taza INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_boli INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_blocNotas INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_llavero INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_usb INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_powerbank INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_gorras INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+    ID_camisas INT,
+    Cantidad INT,
+    PRECIO_TOTAL DECIMAl (10, 2)
+
+
+
+
+
+
+
+
+
 
 );
